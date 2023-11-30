6a63d3ac56a89a241ffb067e88a2b13940522054
nawarian <nickolas@phpsp.org.br>
2023-11-30 08:14:21 +0100

    QrCode via metodo GET

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use image::codecs::jpeg::JpegEncoder;
 use qrcode_generator::QrCodeEcc;
-use serde::{Deserialize, Serialize};
-use std::io::Cursor;
+use std::{collections::HashMap, io::Cursor};
 use worker::{
     event, Context, Cors, Env, Headers, Method, Request, Response, Result, RouteContext, Router,
 };
@@ -25,29 +24,24 @@ async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
     }
 
     let cors = Cors::default()
-        .with_max_age(86400)
         .with_origins(vec![origin])
         .with_allowed_headers(vec!["content-type"])
         .with_exposed_headers(vec!["content-type"])
-        .with_methods(vec![Method::Post, Method::Options]);
+        .with_methods(vec![Method::Get, Method::Options]);
 
     Router::new()
         .options("/v1/qrcode/:format", |_, _| Response::ok("Hello, friend"))
-        .post_async("/v1/qrcode/:format", generate_qrcode)
+        .get_async("/v1/qrcode/:format", generate_qrcode)
         .run(req, env)
         .await?
         .with_cors(&cors)
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
-struct GenerateQrCodeInput {
-    text_content: String,
-    bg_color: String,
-    fg_color: String,
-}
+async fn generate_qrcode(req: Request, ctx: RouteContext<()>) -> Result<Response> {
+    let url = req.url().expect("Failed to fetch request's URL params");
+    let query: HashMap<String, String> = url.query_pairs().into_owned().collect();
 
-async fn generate_qrcode(mut req: Request, ctx: RouteContext<()>) -> Result<Response> {
-    let input = req.json::<GenerateQrCodeInput>().await?;
+    let text_content = query.get("t").expect("Param 't' is mandatory");
 
     let mut headers = Headers::new();
 
@@ -55,27 +49,24 @@ async fn generate_qrcode(mut req: Request, ctx: RouteContext<()>) -> Result<Resp
     let format = ctx.param("format").unwrap_or(&default_fmt).as_str();
     match format {
         "svg" => {
-            let buff = qrcode_generator::to_svg_to_string(
-                input.text_content,
-                QrCodeEcc::Low,
-                256,
-                None::<&str>,
-            )
-            .unwrap();
+            let buff =
+                qrcode_generator::to_svg_to_string(text_content, QrCodeEcc::Low, 256, None::<&str>)
+                    .unwrap();
             let _ = headers.append("content-type", "image/svg");
+            let _ = headers.append("cache-control", "public, max-age=432000, immutable"); // 5 days cache
 
             Ok(Response::ok(buff).unwrap().with_headers(headers))
         }
         "png" => {
-            let buff =
-                qrcode_generator::to_png_to_vec_from_str(input.text_content, QrCodeEcc::Low, 1024)
-                    .unwrap();
+            let buff = qrcode_generator::to_png_to_vec_from_str(text_content, QrCodeEcc::Low, 1024)
+                .unwrap();
             let _ = headers.append("content-type", "image/png");
+            let _ = headers.append("cache-control", "public, max-age=432000, immutable"); // 5 days cache
 
             Ok(Response::from_bytes(buff).unwrap().with_headers(headers))
         }
         "jpg" | "jpeg" => {
-            let img = qrcode_generator::to_image_buffer(input.text_content, QrCodeEcc::Low, 1024)
+            let img = qrcode_generator::to_image_buffer(text_content, QrCodeEcc::Low, 1024)
                 .expect("failed to generate Qr Code.");
 
             let mut bytes: Vec<u8> = Vec::new();
@@ -83,6 +74,7 @@ async fn generate_qrcode(mut req: Request, ctx: RouteContext<()>) -> Result<Resp
             let _ = img.write_with_encoder(encoder);
 
             let _ = headers.append("content-type", "image/jpeg");
+            let _ = headers.append("cache-control", "public, max-age=432000, immutable"); // 5 days cache
 
             Ok(Response::from_bytes(bytes)
                 .expect("Failed to generate Qr Code.")
