6b0d736c1039caa31edb9fa950b5e77969ac16b1
nawarian <nickolas@phpsp.org.br>
2023-11-30 10:26:01 +0100

    Extrai modulo qrcode

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use image::codecs::jpeg::JpegEncoder;
-use qrcode_generator::QrCodeEcc;
-use std::{collections::HashMap, io::Cursor};
+mod qrcode;
+
+use std::collections::HashMap;
 use worker::{
     event, Context, Cors, Env, Headers, Method, Request, Response, Result, RouteContext, Router,
 };
@@ -25,12 +25,10 @@ async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
 
     let cors = Cors::default()
         .with_origins(vec![origin])
-        .with_allowed_headers(vec!["content-type"])
         .with_exposed_headers(vec!["content-type"])
         .with_methods(vec![Method::Get, Method::Options]);
 
     Router::new()
-        .options("/v1/qrcode/:format", |_, _| Response::ok("Hello, friend"))
         .get_async("/v1/qrcode/:format", generate_qrcode)
         .run(req, env)
         .await?
@@ -47,39 +45,34 @@ async fn generate_qrcode(req: Request, ctx: RouteContext<()>) -> Result<Response
 
     let default_fmt = String::from("default");
     let format = ctx.param("format").unwrap_or(&default_fmt).as_str();
-    match format {
-        "svg" => {
-            let buff =
-                qrcode_generator::to_svg_to_string(text_content, QrCodeEcc::Low, 256, None::<&str>)
-                    .unwrap();
-            let _ = headers.append("content-type", "image/svg");
+
+    let payload  = qrcode::QrCodePayload {
+        text: text_content.to_string(),
+        format: match format {
+            "jpg" => qrcode::QrCodeFormat::JPG,
+            "png" => qrcode::QrCodeFormat::PNG,
+            "svg" => qrcode::QrCodeFormat::SVG,
+            _ => qrcode::QrCodeFormat::INVALID,
+        },
+    };
+
+    if matches!(payload.format, qrcode::QrCodeFormat::INVALID) {
+        return Response::error("Invalid image format", 503);
+    }
+
+    let buff = qrcode::generate_qrcode(payload.clone());
+    match buff.is_ok() {
+        true => {
+            let _ = headers.append("content-type", match payload.format {
+                qrcode::QrCodeFormat::JPG => "image/jpeg",
+                qrcode::QrCodeFormat::PNG => "image/png",
+                qrcode::QrCodeFormat::SVG => "image/svg",
+                qrcode::QrCodeFormat::INVALID => "image/invalid",
+            });
             let _ = headers.append("cache-control", "public, max-age=432000, immutable"); // 5 days cache
 
-            Ok(Response::ok(buff).unwrap().with_headers(headers))
-        }
-        "png" => {
-            let buff = qrcode_generator::to_png_to_vec_from_str(text_content, QrCodeEcc::Low, 1024)
-                .unwrap();
-            let _ = headers.append("content-type", "image/png");
-            let _ = headers.append("cache-control", "public, max-age=432000, immutable"); // 5 days cache
-
-            Ok(Response::from_bytes(buff).unwrap().with_headers(headers))
-        }
-        "jpg" | "jpeg" => {
-            let img = qrcode_generator::to_image_buffer(text_content, QrCodeEcc::Low, 1024)
-                .expect("failed to generate Qr Code.");
-
-            let mut bytes: Vec<u8> = Vec::new();
-            let encoder = JpegEncoder::new_with_quality(Cursor::new(&mut bytes), 85);
-            let _ = img.write_with_encoder(encoder);
-
-            let _ = headers.append("content-type", "image/jpeg");
-            let _ = headers.append("cache-control", "public, max-age=432000, immutable"); // 5 days cache
-
-            Ok(Response::from_bytes(bytes)
-                .expect("Failed to generate Qr Code.")
-                .with_headers(headers))
-        }
-        _ => Response::error("Failed to generate Qr Code.", 400),
+            Ok(Response::from_bytes(buff.unwrap()).unwrap().with_headers(headers))
+        },
+        false => Response::error("Something went wrong", 503)
     }
 }
